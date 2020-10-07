602847261fb4ccca9edd13bf68f83b5aa1879250
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-10-07 17:12:52 -0400

    Update charts.js

--- charts.js
+++ charts.js
@@ -15,8 +15,9 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     var firstSample = sampleNames[0];
-    buildCharts(firstSample);
     buildMetadata(firstSample);
+    buildCharts(firstSample);
+  
   });
 }
 
