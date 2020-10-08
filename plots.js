2aed7acc0e56c4ad6ff3fff87204a077e56b8e33
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-10-07 23:24:38 -0400

    Updating Chart

--- plots.js
+++ plots.js
@@ -1,45 +1,37 @@
-function init() {
-  var selector = d3.select("#selDataset");
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-    // Use the first sample from the list to build the initial plots
-    var firstSample = sampleNames[0];
-    buildMetadata(firstSample);
-    buildCharts(firstSample);
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-})}
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
+// data
+var data = [trace1];
 
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    console.log(resultArray);
-    var PANEL = d3.select("#sample-metadata");
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
 
-    PANEL.html("");
-    PANEL.append("h6").text('ID: ' + result.id);
-    PANEL.append("h6").text('ETHNICITY: ' + result.ethnicity);
-    PANEL.append("h6").text('GENDER: ' + result.gender);
-    PANEL.append("h6").text('LOCATION: ' + result.location);
-    PANEL.append("h6").text('AGE: ' + result.age);
-    PANEL.append("h6").text('BBTYPE: ' + result.bbtype);
-    PANEL.append("h6").text('WFREQ: ' + result.wfreq);
-  });
-}
-
-init();
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
