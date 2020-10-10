be6d807b5de6345b77e8a95f1d7ecd71deb327d6
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-10-09 23:47:04 -0400

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -61,7 +61,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("../../samples.json").then((data) => {
 
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
